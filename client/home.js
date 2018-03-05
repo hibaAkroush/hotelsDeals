88506e90ad25718bd6119f2c5ea63739cf77626e
Hiba Akroush <HAkroush@optimizasolutions.com>
2018-03-05 20:55:55 +0200

    final commit

--- client/home.js
+++ client/home.js
@@ -1,15 +1,16 @@
 // assumption: this function sends an ajax request to the server and in case of success it appends 
 // the data to the DOM in case of error it alerts the error
 function app() {
+    //making a GET request to /data/hotels endpoint to get data
     $.ajax({
         url: 'http://localhost:9000/data/hotels',
         cache: false,
         type: 'GET',
         success: function(result) {
-            console.log(result)
             var hotels = JSON.parse(result);
+
+            //iterating over results and appending them to the DOM
             _.each(hotels, function(val, i) {
-                console.log(val)
                 var box = '<div id = "box"></div>';
                 var hotelImage = '<img src= ' + '"' + val.hotelInfo.hotelImageUrl + '"' + 'id="hotelImage"/>';
                 var starLogo = '<img src= "star.png" id="starLogo"/>';
@@ -22,6 +23,7 @@ function app() {
                 var averagePriceValue = '<p id="averagePriceValue">' + Math.ceil(val.hotelPricingInfo.averagePriceValue) + '</p>';
                 var crossOutPriceValue = '<p id="crossOutPriceValue" >' + Math.ceil(val.hotelPricingInfo.crossOutPriceValue) + '</p>';
                 var percentSavings = '<p id="percentSavings">Save ' + Math.ceil(val.hotelPricingInfo.percentSavings) + '%' + '</p>';
+
                 $('#hotel' + i).append(
                     hotelImage, 
                     box, 
--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
-const express = require('express')
+
+const express = require('express');
+
+//creating app
 const app = express();
 module.exports = app;
+
 const request = require('request');
 var bodyParser = require('body-parser');
 var jsonminify = require("jsonminify");
 var rp = require('request-promise');
 var cors = require('cors');
+var path = require('path');
 
 // Parse JSON (uniform resource locators)
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname + '/../client')); 
 app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 app.use('/slick', express.static(__dirname + '/../slick'));
-var path = require('path');
 
 app.listen(9000, function() {
     console.log('Application is listening on 9000');
@@ -24,32 +27,25 @@ app.listen(9000, function() {
 // this code sends a call to Expedia's API if it succeeds the data will be send to front end 
 var hotels;
 app.get('/data/hotels', function(req, res) {
+
+    //defining configs for data request
     var options = {
         uri: 'https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel',
         json: true
     };
 
     rp(options)
-        .then(function(body) {
-            hotels = JSON.stringify(body.offers.Hotel);
-
-            res.send(hotels);
-        })
-        .catch(function(err) {
-            console.log("An Error Occured: ", err);
-        });
+    .then(function(body) {
+        //sending the data back
+        hotels = JSON.stringify(body.offers.Hotel);
+        res.send(hotels);
+    })
+    .catch(function(err) {
+        console.log("An Error Occured: ", err);
+    });
 });
 
 app.get('/', function(req, res){
+    //sending the main screen
     res.sendFile(path.join(__dirname + '/../client/main.html'));
 })
-
-// this function was written for debugging 
-function isJson(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
